6fd9936cc517915042ecf6133644649729082e3a
aryanluffy <aryanchauhan5003@gmail.com>
2020-12-05 01:03:33 +0530

    Early Stopping done

--- BasicLSTM.py
+++ BasicLSTM.py
@@ -4,15 +4,16 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
 
-data = pd.read_csv('GOOG.csv', date_parser = True)
+data = pd.read_csv('SBIN.csv', date_parser = True)
 data.tail()
 
 data_training = data[data['Date']<'2018-12-31'].copy()
 data_test = data[data['Date']>='2018-12-31'].copy()
 # print(data_test)
 
-data_training = data_training.drop(['Date','High','Low'], axis = 1)
+data_training = data_training.drop(['Date'], axis = 1)
 # print(data_training)
 scaler = MinMaxScaler()
 data_training = scaler.fit_transform(data_training)
@@ -25,28 +26,28 @@ for i in range(20, data_training.shape[0]):
     X_train.append(data_training[i-20:i])
     y_train.append(data_training[i, 0])
 
-data_training=pd.DataFrame(data_training,columns=['Open','Close','Adj Close','Volume'])
+data_training=pd.DataFrame(data_training,columns=['Open','High','Low','Close','Adj Close','Volume'])
 
 X_train, y_train = np.array(X_train), np.array(y_train)
 X_train.shape
 
 regressor = Sequential()
-
+# simple early stopping
 # 75's results were good
-regressor.add(LSTM(units = 100, activation = 'tanh', recurrent_activation='sigmoid', input_shape = (X_train.shape[1], 4)))
-regressor.add(Dropout(0.2))
+regressor.add(LSTM(units = 100,activation = 'tanh', recurrent_activation='sigmoid', input_shape = (X_train.shape[1], 6)))
 
 regressor.add(Dense(units = 1))
 regressor.summary()
 
 regressor.compile(optimizer='adam', loss = 'mean_squared_error')
-regressor.fit(X_train, y_train, epochs=66, batch_size=30)
-
+es = EarlyStopping(monitor='loss',patience=20,verbose=0,restore_best_weights=True)
+regressor.fit(X_train, y_train, epochs=50, batch_size=30,verbose=0,callbacks=[es])
+# regressor.fit(X_train, y_train, epochs=100, batch_size=30)
 
 past_60_days = data_training.tail(20)
 print(len(data_test))
 df = past_60_days.append(data_test, ignore_index = True)
-df = df.drop(['Date','High','Low'], axis = 1)
+df = df.drop(['Date'], axis = 1)
 df.head()
 print(len(df))
 inputs = scaler.transform(df)
