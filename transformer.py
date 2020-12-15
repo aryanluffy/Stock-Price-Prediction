279dfe6dc97a2d834f5630f7e226d23671e78016
aryanluffy <aryanchauhan5003@gmail.com>
2020-12-15 17:31:08 +0530

    tuning

--- transformer.py
+++ transformer.py
@@ -22,10 +22,10 @@ from tensorflow.keras.layers import *
 
 batch_size = 32
 seq_len = 20
-d_k = 64
-d_v = 64
+d_k = 48
+d_v = 48
 n_heads = 6
-ff_dim = 64
+ff_dim = 48
 
 class Time2Vector(Layer):
   def __init__(self, seq_len, **kwargs):
@@ -172,7 +172,7 @@ class TransformerEncoder(Layer):
 def create_model():
   '''Initialize time and transformer layers'''
   time_embedding = Time2Vector(seq_len)
-  # attn_layer1 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
+  attn_layer1 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
   # attn_layer2 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
   # attn_layer3 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
 
@@ -180,12 +180,12 @@ def create_model():
   in_seq = Input(shape=(seq_len, 13))
   x = time_embedding(in_seq)
   x = Concatenate(axis=-1)([in_seq, x])
-  # x = attn_layer1((x, x, x))
+  x = attn_layer1((x, x, x))
   # x = attn_layer2((x, x, x))
   # x = attn_layer3((x, x, x))
   x = GlobalAveragePooling1D(data_format='channels_first')(x)
 #   x = Dropout(0.1)(x)
-#   x = Dense(64)(x)
+  x = Dense(64)(x)
 #   x = Dropout(0.1)(x)
   out = Dense(1)(x)
 
